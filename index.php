8d5b518423858669e2a90e8fe9ebd95f09a9f047
HardCarroll <phanchan_hc@163.com>
2018-12-17 15:40:10 +0800

    modify 403&404 pages

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>积硅步，以至千里</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+  
+</body>
+</html>
