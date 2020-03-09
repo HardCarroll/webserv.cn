41bc5cacb8e5dd4882fa558653b4e979b73481c2
HardCarroll <phanchan_hc@163.com>
2020-03-09 13:44:23 +0800

    add link to gov

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
-  
+  <a href="http://www.beian.miit.gov.cn" target="_blank"><img src="/include/images/icp.jpg">湘ICP备18017808号-2</a>
 </body>
 </html>
