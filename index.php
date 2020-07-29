080a79fd184df4d8c4b28d7c6034e9e8dc9cae48
HardCarroll <phanchan_hc@163.com>
2020-07-29 19:30:53 +0800

    upload 0729

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<?php
+$article_json = '[{"url": "/colorboard/index.php", "title": "调色板丨多种颜色，总有一种是你想要的"}]';
+$article_array = json_decode($article_json, TRUE);
+?>
 <!DOCTYPE html>
 <html lang="zh-CN">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
@@ -8,8 +13,40 @@
   <meta name="Description" content="">
   <title>积硅步，以至千里</title>
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="/library/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/include/css/home.min.css">
 </head>
+
 <body>
-  <a href="http://www.beian.miit.gov.cn" target="_blank"><img src="/include/images/icp.jpg">湘ICP备18017808号-2</a>
+  <div class="layer">
+    <div class="index glyphicon">
+      <div class="inner">
+        <?php
+        echo '<ul class="article">';
+        for($i=0; $i<count($article_array); $i++) {
+          echo '<li class="text-ellipsis item"><a href="'.$article_array[$i]["url"].'">'.$article_array[$i]["title"].'</a></li>';
+        }
+        echo '</ul>';
+        ?>
+      </div>
+    </div>
+    <div class="hello">
+      <span>hello</span>
+      <span>world</span>
+    </div>
+    <div class="footer">
+      <a href="http://www.beian.miit.gov.cn" target="_blank"><img src="/media/images/icp.jpg">湘ICP备18017808号-2</a>
+    </div>
+  </div>
 </body>
+
+<script src="/library/jquery/jquery.min.js"></script>
+<script>
+  $(function() {
+    $(".index").off("click").on("click", function() {
+      $(this).toggleClass("active")
+    });
+  });
+</script>
+
 </html>
--- library/plugin/fileupload/fileupload.js
+++ library/plugin/fileupload/fileupload.js
@@ -1,48 +1,68 @@
-;(function($){
-  var defaults = {cssFile: "/library/plugin/fileupload/fileupload.css", inputId: "#fileUpload", debug: false, callback: ""};
+;(function ($) {
+  var js = document.scripts;
+  var path = js[js.length - 1].src.substring(0, js[js.length - 1].src.lastIndexOf("/") + 1);
+  var domain = "http://" + document.domain;
+  var realPath = path.replace(domain, "");
+  var defaults = { inputId: "#fileUpload", modalId: "#uploadModal", uploadOnSelect: false, callback: "" };
   var methods = {
-    init: function(options) {
+    init: function (options) {
       // call init();
       var settings = $.extend({}, defaults, options);
+      settings.filePath = realPath;
 
-      return $(this).each(function() {
-        settings.this = $(this);
-        // var _this = $(this);
+      return $(this).each(function () {
+        // save DOM nodes
+        settings.files = [];
+
         // load css file
-        var bCssFile = $("head").find("link[href='"+ defaults.cssFile +"']").length;
-        if(!bCssFile) {
-          $("head").append('<link rel="stylesheet" href="'+defaults.cssFile+'">');
-        }
-        bCssFile = $("head").find("link[href='"+ settings.cssFile +"']").length;
-        if(!bCssFile) {
-          $("head").append('<link rel="stylesheet" href="'+settings.cssFile+'">');
-        }
-        // add buttons
-        settings.this.append('<div class="button"><div class="btn btn-default btn-select" data-target="'+settings.inputId+'">选择文件</div><div class="btn btn-success btn-upload">开始上传</div></div>');
-        if(!$("body").find(settings.inputId).length) {
-          $("body").append('<input type="file" id="'+settings.inputId.slice(1)+'" class="hidden" multiple>');
+        bCssFile = $("head").find("link[href='" + settings.filePath + "fileupload.css']").length;
+        if (!bCssFile) {
+          $("head").append('<link rel="stylesheet" href="' + settings.filePath + 'fileupload.css">');
         }
 
-        settings.this.find(".btn-select").off("click").on("click", function() {
+        // add input control to body if there is no input of inputId;
+        var input = '<input type="file" id="' + settings.inputId.slice(1) + '" class="hidden" multiple>';
+        if (!$("body").find(settings.inputId).length) {
+          $("script").eq(0).before(input);
+        }
+
+        // add modal control to body if there is no modal of modalId;
+        var modal = '<div class="modal" id="' + settings.modalId.slice(1) + '" tabindex="-1" role="dialog"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">文件上传</div><div class="modal-body"></div><div class="modal-footer"><span class="tips">总共*个文件，*个已成功上传，*个上传失败！</span><button type="button" class="btn btn-default" data-dismiss="modal">关闭</button><button type="button" class="btn btn-primary btn-upload">上传</button></div></div></div></div>';
+        if (!$("body").find(settings.modalId).length) {
+          $("script").eq(0).before(modal);
+          $(settings.modalId).on('hidden.bs.modal', function () {
+            // empty all elements in .modal-body
+            settings.files = [];
+            $(this).find(".modal-body").empty();
+          }).find(".btn-upload").off("click").on("click", function () {
+            // starting upload files
+            if (settings.files.length) {
+              for (var i = 0; i < settings.files.length; i++) {
+                uploadFiles(settings, i);
+              }
+            }
+            else {
+              console.log("you didn't select any file!");
+            }
+          });
+        }
+
+        $(this).addClass("glyphicon glyphicon-file").css({"cursor": "pointer"}).off("click").on("click", function () {
+          // select any file you want to upload
           selectFiles(settings);
         });
 
-        settings.this.find(".btn-upload").off("click").on("click", function() {
-          uploadFiles(settings);
-        });
       });
-    },
-    debug: function(msg) {
-      console.log(msg);
     }
+
   };
 
-  $.fn.fileUpload = function(method) {
+  $.fn.fileUpload = function (method) {
     // 方法调用
-    if(methods[method]) {
+    if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
     }
-    else if(typeof method === 'object' || !method) {
+    else if (typeof method === 'object' || !method) {
       return methods.init.apply(this, arguments);
     }
     else {
@@ -51,10 +71,10 @@
   };
 
   function createObjectURL(file) {
-    if(window.webkitURL) {
+    if (window.webkitURL) {
       return window.webkitURL.createObjectURL(file);
     }
-    else if(window.URL && window.URL.createObjectURL) {
+    else if (window.URL && window.URL.createObjectURL) {
       return window.URL.createObjectURL(file);
     }
     else {
@@ -64,16 +84,18 @@
 
   function selectFiles(settings) {
     $(settings.inputId).click().off("change").on("change", function(e) {
-      settings.this.find(".list").remove();
+      var box = $(settings.modalId).find(".modal-body");
       var ele = $('<div class="list"></div>');
       var files = settings.files = Array.prototype.slice.call(e.target.files);
       for(var i=0; i<files.length; i++) {
         var item = $('<div class="list-item"><div class="controls"></span><span class="glyphicon glyphicon-trash"></span></div><div class="progress hidden"><div class="progress-bar progress-bar-success"></div></div><div class="info"><span class="name"></span></div></div>');
         item.appendTo(ele).css({"background-image": "url("+createObjectURL(files[i])+")"}).find("span.name").html(files[i].name);
       }
-      ele.appendTo(settings.this);
+      box.empty().append(ele);
 
       ele.find("span.glyphicon-trash").off("click").on("click", function() {
+        // change array of files, delete current index of item
+        // if delete all files, then make files null
         files.splice($(this).parent().parent().index(), 1);
         if(files.length) {
           // remove div.list-item
@@ -82,31 +104,48 @@
         else {
           // remove div.list
           $(this).parent().parent().parent().remove();
+          $(settings.modalId).modal("hide");
         }
 
         settings.files = files;
       });
+
+      if(settings.files.length) {
+        if (settings.uploadOnSelect) {
+          for (var i = 0; i < settings.files.length; i++) {
+            uploadFiles(settings, i);
+          }
+        }
+        else {
+          // open modal of modalId
+          $(settings.modalId).modal({
+            backdrop: 'static',
+            keyboard: false
+          });
+        }
+      }
+      
+      // 解决选择相同文件不触发change事件
+      e.target.value = null;
+
     });
   }
 
-  function uploadFiles(settings) {
-    // console.log(settings);
-    // settings.this.find(".list-item").eq(0).find(".progress").removeClass("hidden");
-    settings.this.find(".progress").removeClass("hidden");
+  function uploadFiles(settings, index = 0) {
+    var pNode = $(settings.modalId).find(".list-item").eq(index);
+    pNode.find(".progress").removeClass("hidden");
+    pNode.find("span.name").html("");
 
     var fmd = new FormData();
-    for(var i=0; i<settings.files.length; i++) {
-      // fmd.append("files["+i+"]", settings.files[i]);
-    // }
-    fmd.append("files", settings.files[i]);
+    fmd.append("uploadFiles", settings.files[index]);
+    fmd.append("token", "uploadFiles");
     $.ajax({
-      url: "/library/plugin/fileupload/fileupload.php",
+      url: settings.filePath + "fileupload.php",
       type: "POST",
       data: fmd,
       processData: false,
       contentType: false,
       dataType: "json",
-      // context: settings.this.find(".list-item").eq(i),
       xhr: function () {
         myxhr = $.ajaxSettings.xhr();
         if (myxhr.upload) {
@@ -114,21 +153,22 @@
             var loaded = e.loaded;
             var total = e.total;
             var percent = Math.floor(loaded / total * 100);
-            $(".progress-bar").css("width", percent + "%");
-            
+            pNode.find(".progress-bar").css("width", percent + "%");
           }, false);
         }
         return myxhr;
       },
       success: function (ret) {
-        // $(this).find(".progress").addClass("hidden").parent().next().find(".progress").removeClass("hidden");
-        console.log(ret);
+        // console.log(ret);
+        if(ret.err_code) {
+          pNode.find(".progress-bar").toggleClass("progress-bar-success").toggleClass("progress-bar-danger");
+        }
+        pNode.find("span.name").html(ret.err_msg);
       },
       error: function (err) {
         console.log(err);
       }
     });
-  }
 
   }
 
